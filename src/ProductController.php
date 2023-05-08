476c2d616ec42747439924727184df6c04cae445
Winstein Martins <sriwinstein@gmail.com>
2023-05-08 07:54:01 -0300

    :sparkles: feat: add product
    Adding produdct to database

--- src/ProductController.php
+++ src/ProductController.php
@@ -9,7 +9,11 @@ class ProductController {
         if($method == "GET") {
             echo json_encode($this->gateway->getAll());
         } elseif ($method == "POST") {
-            echo "add new";
+            $product = (array) json_decode(file_get_contents("php://input"), true);
+
+            $id = $this->gateway->create($product);
+            http_response_code(201);
+            echo json_encode(["message" => "Product added.", "id" => $id]);
         } else {
             http_response_code(405);
             header("Allow: GET, POST");
--- src/ProductGateway.php
+++ src/ProductGateway.php
@@ -16,4 +16,45 @@ class ProductGateway {
 
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
+
+    public function create(array $product): string
+    {
+        $sql = "INSERT INTO product (sku, name, price, type, weight, size, height, width, length)
+                VALUES (:sku, :name, :price, :type, :weight, :size, :height, :width, :length)";
+
+        $stmt = $this->conn->prepare($sql);
+        $stmt->bindValue(":sku", $product["sku"], PDO::PARAM_STR);
+        $stmt->bindValue(":name", $product["name"], PDO::PARAM_STR);
+        $stmt->bindValue(":price", $product["price"], PDO::PARAM_INT);
+        $stmt->bindValue(":type", $product["type"], PDO::PARAM_STR);
+        
+        if ($product["type"] == "book")
+        {
+            $stmt->bindValue(":weight", $product["weight"], PDO::PARAM_INT);
+            $stmt->bindValue(":size", null, PDO::PARAM_NULL);
+            $stmt->bindValue(":height", null, PDO::PARAM_NULL);
+            $stmt->bindValue(":width", null, PDO::PARAM_NULL);
+            $stmt->bindValue(":length", null, PDO::PARAM_NULL);
+                
+        } elseif ($product["type"] == "dvd")
+        {
+            $stmt->bindValue(":weight", null, PDO::PARAM_NULL);
+            $stmt->bindValue(":size", $product["size"], PDO::PARAM_INT);
+            $stmt->bindValue(":height", null, PDO::PARAM_NULL);
+            $stmt->bindValue(":width", null, PDO::PARAM_NULL);
+            $stmt->bindValue(":length", null, PDO::PARAM_NULL);
+            
+        } elseif ($product["type"] == "furniture")
+        {
+            $stmt->bindValue(":weight", null, PDO::PARAM_NULL);
+            $stmt->bindValue(":size", null, PDO::PARAM_NULL);
+            $stmt->bindValue(":height", $product["height"], PDO::PARAM_INT);
+            $stmt->bindValue(":width", $product["width"], PDO::PARAM_INT);
+            $stmt->bindValue(":length", $product["length"], PDO::PARAM_INT);
+        }
+
+        $stmt->execute();
+
+        return $this->conn->lastInsertId();
+    }
 }
