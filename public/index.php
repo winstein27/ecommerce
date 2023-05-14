adeb577adbda9e3b2bdc4ee0dfd727c771531e76
Winstein Martins <sriwinstein@gmail.com>
2023-05-14 09:09:36 -0300

    :sparkles: feat: error handler
    Adding error and exception handlers

--- public/index.php
+++ public/index.php
@@ -3,6 +3,10 @@
 // declare(strict_types=1);
 
 require_once("../vendor/autoload.php");
+require_once("../App/Core/ErrorHandler.php");
+
+set_error_handler("ErrorHandler::handleError");
+set_exception_handler("ErrorHandler::handleException");
 
 if (file_exists('../.env')) {
     $dotenv = \Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
